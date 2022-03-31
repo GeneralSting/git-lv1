5445ad076c40fe42e0cdae8bde4eb9c05a5ee340
Pavo Skocibusic <pavo.skocibusic@vuv.hr>
2022-03-31 10:40:18 +0200

    drugi commit

--- js/app.js
+++ js/app.js
@@ -13,9 +13,23 @@ function onAddWebsite(e) {
           <td>${semestar}</td>
           <td>${ects}</td>
           <td><button type="button" class="deleteBtn"><i class="fas fa-trash"></i></button></td>
+          <td><input type="checkbox" class="promjeniBoju">
       </tr>
   `;
 }
+
+$(document).ready(function () {
+    $('tr').click(function () {
+        //Check to see if background color is set or if it's set to white.
+        if(this.style.background == "" || this.style.background =="white") {
+            $(this).css('background', 'red');
+        }
+        else {
+            $(this).css('background', 'white');
+        }
+    });
+});
+
 
 function onDeleteRow(e) {
   if (!e.target.classList.contains("deleteBtn")) {
@@ -25,6 +39,15 @@ function onDeleteRow(e) {
   const btn = e.target;
   btn.closest("tr").remove();
 }
+function onChangeRow(e) {
+  if (!e.target.classList.contains("promjeniBoju")) {
+    return;
+  }
+
+  const btn = e.target;
+  btn.closest("tr").style.color = "red";
+}
 
 formEl.addEventListener("submit", onAddWebsite);
 tableEl.addEventListener("click", onDeleteRow);
+tableEl.addEventListener("click", onChangeRow);

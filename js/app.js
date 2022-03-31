0d603de152a048474494b34c8a7181418f0a37cf
Pavo Skocibusic <pavo.skocibusic@vuv.hr>
2022-03-31 09:57:44 +0200

    prvi commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const formEl = document.querySelector("form");
+const tbodyEl = document.querySelector("tbody");
+const tableEl = document.querySelector("table");
+function onAddWebsite(e) {
+  e.preventDefault();
+  const naziv = document.getElementById("naziv").value;
+  const semestar = document.getElementById("semestar").value;
+  const ects = document.getElementById("ects").value;
+
+  tbodyEl.innerHTML += `
+      <tr>
+          <td>${naziv}</td>
+          <td>${semestar}</td>
+          <td>${ects}</td>
+          <td><button type="button" class="deleteBtn"><i class="fas fa-trash"></i></button></td>
+      </tr>
+  `;
+}
+
+function onDeleteRow(e) {
+  if (!e.target.classList.contains("deleteBtn")) {
+    return;
+  }
+
+  const btn = e.target;
+  btn.closest("tr").remove();
+}
+
+formEl.addEventListener("submit", onAddWebsite);
+tableEl.addEventListener("click", onDeleteRow);
